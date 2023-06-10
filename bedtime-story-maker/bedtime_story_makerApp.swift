14855f66f2bcce2a04a73b3610562841a7f658f1
Connor Holland <connoriosdev22@gmail.com>
2023-06-10 13:45:36 -0400

    Initial project setup

---
+++ bedtime-story-maker/bedtime_story_makerApp.swift
@@ -0,0 +1,17 @@
+//
+//  bedtime_story_makerApp.swift
+//  bedtime-story-maker
+//
+//  Created by Connor Holland on 6/10/23.
+//
+
+import SwiftUI
+
+@main
+struct bedtime_story_makerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
